030b30c2929dadd33299512d0227b955ef0e2e2c
Divya Mohan <divyamohan1993@users.noreply.github.com>
2024-04-07 20:54:37 +0530

    Delete src/dmjone/kaustuv_common.py
    Signed-off-by: Divya Mohan <divyamohan1993@users.noreply.github.com>

--- src/dmjone/kaustuv_common.py
+++
@@ -1,8 +0,0 @@
-def calculate_absolute():
-    num = -10
-    absolute_value = abs(num)
-    print(f"Absolute value of {num} is {absolute_value}")
-
-def check_all_true():
-    list1 = [True, True, True]
-    print(all(list1))
