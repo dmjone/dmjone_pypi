07cb650edfb2a6fa4dc098689947eea747c62e13
Divya Mohan <divyamohan1993@users.noreply.github.com>
2024-04-07 19:43:19 +0530

    Update __init__.py
    Signed-off-by: Divya Mohan <divyamohan1993@users.noreply.github.com>

--- src/dmjone/__init__.py
+++ src/dmjone/__init__.py
@@ -6,3 +6,9 @@ from .StudentManagementSystem import projectsms
 
 # Section: Aarushi Sharma
 from .aarushi_TodoList import TodoList
+
+# Section: Kaustuv Sharma
+import src.dmjone.kaustuv_common as kaustuv
+import src.dmjone.kaustuv_function2 as kaustuv_function2
+
+kaustuv.__dict__.update(kaustuv_function2.__dict__)
